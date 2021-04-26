73db7c62d7d668cba0cced6cd71f80281f1646e0
billy <rain2desert@hotmail.com>
2021-04-26 11:50:50 +0800

    hibernate test

---
+++ src/test/java/com/example/demo_hibernate/DemoHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
